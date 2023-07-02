b0e1aeaf0523d0de493264337e4ef883db257ec3
unknown <safaeimobina1381@gmail.com>
2023-07-02 16:29:09 +0430

    Update Usercard

--- src/components/users/UserCard.vue
+++ src/components/users/UserCard.vue
@@ -2,7 +2,8 @@
   <div>
     <div class="card" style="width: 18rem;">
       <div class="card-header">
-        <router-link :to="{name:'showuser', params: { id: user.id } }">{{user.name}}</router-link>
+        <router-link :to="{name:'showuser', params: { id: user.id } }" v-if="!idExist">{{user.name}}</router-link>
+        <li v-else class="list-group-item">{{ user.name }}</li>
       </div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">{{ user.username }}</li>
@@ -14,9 +15,24 @@
 </template>
 
 <script>
-// import { useRoute } from "vue-router";
+import { useRoute } from "vue-router";
 export default {
-  props: { user: Object }
+  props: { user: Object },
+  data() {
+    return {
+      idExist: false
+    };
+  },
+  mounted() {
+    this.changeIdStatus();
+  },
+  methods: {
+    changeIdStatus() {
+      if (useRoute().params.id !== undefined) {
+        this.idExist = true;
+      }
+    }
+  }
 };
 </script>
 
