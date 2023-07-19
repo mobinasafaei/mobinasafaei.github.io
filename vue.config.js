f76b60c207ce4a2c9c40db13da50f4600a843da3
unknown <safaeimobina1381@gmail.com>
2023-07-19 19:20:00 +0330

    modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-mini-project/'
-    : '/'
+  publicPath:'/vue-mini-project/'
+    
 })
